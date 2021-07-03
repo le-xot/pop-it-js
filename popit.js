057b2c93f56053ca61183491717f245d37bf7a52
le-xot <ShvAlesha2000@yandex.ru>
2021-07-04 04:17:30 +0500

    fix

--- popit.js
+++ popit.js
@@ -1,3 +1,78 @@
+class Swipe {
+  constructor(element) {
+      this.xDown = null;
+      this.yDown = null;
+      this.element = typeof(element) === 'string' ? document.querySelector(element) : element;
+
+      this.element.addEventListener('touchstart', function(evt) {
+          this.xDown = evt.touches[0].clientX;
+          this.yDown = evt.touches[0].clientY;
+      }.bind(this), false);
+
+  }
+
+  onLeft(callback) {
+      this.onLeft = callback;
+
+      return this;
+  }
+
+  onRight(callback) {
+      this.onRight = callback;
+
+      return this;
+  }
+
+  onUp(callback) {
+      this.onUp = callback;
+
+      return this;
+  }
+
+  onDown(callback) {
+      this.onDown = callback;
+
+      return this;
+  }
+
+  handleTouchMove(evt) {
+      if ( ! this.xDown || ! this.yDown ) {
+          return;
+      }
+
+      var xUp = evt.touches[0].clientX;
+      var yUp = evt.touches[0].clientY;
+
+      this.xDiff = this.xDown - xUp;
+      this.yDiff = this.yDown - yUp;
+
+      if ( Math.abs( this.xDiff ) > Math.abs( this.yDiff ) ) { // Most significant.
+          if ( this.xDiff > 0 ) {
+              this.onLeft();
+          } else {
+              this.onRight();
+          }
+      } else {
+          if ( this.yDiff > 0 ) {
+              this.onUp();
+          } else {
+              this.onDown();
+          }
+      }
+
+      // Reset values.
+      this.xDown = null;
+      this.yDown = null;
+  }
+
+  run() {
+      this.element.addEventListener('touchmove', (evt) => {
+          this.handleTouchMove(evt)
+      }, false);
+  }
+}
+
+const swiper = new Swipe('.contain')
 const pop1 = document.querySelector('.sound1')
 const pop2 = document.querySelector('.sound2')
 let number = randomInteger(1, 2)
@@ -31,18 +106,17 @@ function snd(number) {
 
 buttons.forEach(button => button.addEventListener('click', () => { snd(randomInteger(1, 2)) }))
 document.querySelector('.flip').onclick = () => {
-
   const columns = document.querySelectorAll('.column')
   reverseColumns()
   buttons.forEach(button => button.classList.toggle('pressed'))
 }
 
-
-class Popit {
-  constructor() {
-
-  }
-
-
+function flipPopit() {
+  const columns = document.querySelectorAll('.column')
+  reverseColumns()
+  buttons.forEach(button => button.classList.toggle('pressed'))
 }
 
+swiper.onLeft(() => flipPopit())
+swiper.onRight(() => flipPopit())
+swiper.run()
